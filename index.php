821525e948760c51d493810dd47c34b8a232283f
pedroryansc <pedroryangamer@gmail.com>
2024-05-13 15:39:23 -0300

    Continuação 2 do HTML e CSS

--- index.php
+++ index.php
@@ -33,13 +33,24 @@
                     <td>18/10/2004</td>
                     <td>Masculino</td>
                     <td>Ensino Médio completo</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><a href=""><img src="img/visualizar.png" alt="Visualizar o perfil da pessoa" width="32px"></a></td>
+                    <td><a href=""><img src="img/editar.png" alt="Editar o perfil da pessoa" width="32px"></a></td>
+                    <td><a href=""><img src="img/remover.png" alt="Remover o perfil da pessoa" width="32px"></a></td>
+                </tr>
+                <tr>
+                    <td>123.456.789-10</td>
+                    <td>Pedro Henrique Damann</td>
+                    <td>14/01/2005</td>
+                    <td>Masculino</td>
+                    <td>Ensino Médio completo</td>
+                    <td><a href=""><img src="img/visualizar.png" alt="Visualizar o perfil da pessoa" width="32px"></a></td>
+                    <td><a href=""><img src="img/editar.png" alt="Editar o perfil da pessoa" width="32px"></a></td>
+                    <td><a href=""><img src="img/remover.png" alt="Remover o perfil da pessoa" width="32px"></a></td>
                 </tr>
             </table>
         </section>
     </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <?php
         include "footerRoxo.html";
     ?>
